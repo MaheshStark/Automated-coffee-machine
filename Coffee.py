e619d2c0d929059bc6dae0c598b5c0704a5ecc3e
MaheshStark <mbkmahesh1996@gmail.com>
2020-03-25 17:10:47 +0530

    Done

--- Coffee.py
+++ Coffee.py
@@ -9,28 +9,12 @@ class Coffee:
     def __init__(self, coffeType, country):  # constructor for the first mixture
         self.coffeType = coffeType
         self.country = country
+
+    def completeMadeTheCoffee(self):
         Coffee.water -= 1
         Coffee.suger -= 1
         Coffee.coffePowder -= 1
-    #
-    # def __init__(self, coffeType, country, additionalCoffee):  # constructor for black coffee
-    #     Coffee.coffePowder -= 1
-    #     cLast = BlackCoffee(self.typeName, self.country,additionalCoffee)
 
-    # def __init__(self, coffeType, country, milk, additionalCoffee, additionalMilk):  # constructor for Cappuccino
-    #     Coffee.milk -= 2
-    #     Coffee.coffePowder -= 1
-    #     cLast = Cappuccino(self.typeName, self.country, milk, additionalCoffee, additionalMilk)
-    #
-    # def __init__(self, coffeType, country, milk, additionalMilk):  # constructor for Milk Coffee
-    #     Coffee.milk -= 2
-    #     cLast = MilkCoffee(self.typeName, self.country, milk, additionalMilk)
-    #
-    # def __init__(self, coffeType, country, milk, ice, additionalMilk, additionalSugur):  # constructor for Ice Coffee
-    #     Coffee.milk -= 2
-    #     Coffee.ice -= 1
-    #     Coffee.suger -= 1
-    #     cLast = IceCoffee(self.typeName, self.country, milk, ice, additionalMilk, additionalSugur)
 
 class BlackCoffee(Coffee):
     def __init__(self, coffeType, country, additionalCoffee):
@@ -58,6 +42,7 @@ class MilkCoffee(Coffee):
         self.milk = milk
         self.additionalMilk = additionalMilk
         Coffee.milk -= 2
+
 
 class IceCoffee(Coffee):
     def __init__(self, coffeType, country, milk, ice, additionalMilk, additionalSugur):
--- CoffeeMaker.py
+++ CoffeeMaker.py
@@ -1,10 +1,13 @@
+from filecmp import cmp
+
 import Coffee
 
 moneyInput = 0
-sugur=1
-milk=1
-ice=1
-coffeePowder=1
+sugur = 1
+milk = 1
+ice = 1
+coffeePowder = 1
+completeBusiness = False
 print("|---------------------------------------------------------------------|")
 print("|Coffee types---------->>>                                            |")
 print("|    - 1.Black coffee                     2.Cappuccino                |")
@@ -12,53 +15,68 @@ print("|    - 3.Milk Coffee                      4.Ice Coffee                |")
 print("|                                                          100 Rs each|")
 print("|---------------------------------------------------------------------|")
 
-
 print("|---------------------------------------------------------------------|")
 print("|Available ingrediens---------->>>                                    |")
-print("|    - 1.coffee -" + str(Coffee.Coffee.coffePowder) +"                    2.Milk -"+ str(Coffee.Coffee.milk) +"                     |")
-print("|    - 3.ice    -" + str(Coffee.Coffee.ice)+"                    4.water-"+ str(Coffee.Coffee.water) +"                     |")
-print("|    - 3.sugur  -" + str(Coffee.Coffee.suger) +"                                                   |")
+print("|    - 1.coffee -" + str(Coffee.Coffee.coffePowder) + "                    2.Milk -" + str(
+    Coffee.Coffee.milk) + "                     |")
+print("|    - 3.ice    -" + str(Coffee.Coffee.ice) + "                    4.water-" + str(
+    Coffee.Coffee.water) + "                     |")
+print("|    - 3.sugur  -" + str(Coffee.Coffee.suger) + "                                                   |")
 print("|---------------------------------------------------------------------|")
 
-typeCoffee = int(input('What is the coffee type you want: '))
-if typeCoffee == 1:
-    typeName = "Black coffee"
-elif typeCoffee == 2:
-    typeName = "Cappuccino"
-elif typeCoffee == 3:
-    typeName = "Milk Coffee"
-elif typeCoffee == 4:
-    typeName = "Ice Coffee"
+while completeBusiness==False:
+    typeCoffee = int(input('What is the coffee type you want: '))
+    if typeCoffee == 1:
+        typeName = "Black coffee"
+    elif typeCoffee == 2:
+        typeName = "Cappuccino"
+    elif typeCoffee == 3:
+        typeName = "Milk Coffee"
+    elif typeCoffee == 4:
+        typeName = "Ice Coffee"
 
-country = input('Which country made coffee you want: ')
+    country = input('Which country made coffee you want: ')
 
-while Coffee.Coffee.price > moneyInput:
-    money = input('Add the enough Money: ')
-    moneyInput += int(money)
-    if moneyInput > Coffee.Coffee.price:
-        returnMoney = moneyInput - Coffee.Coffee.price
-        print('Take your return Money:' + str(returnMoney) + 'Rs')
-# makeCoffee()
+    while Coffee.Coffee.price > moneyInput:
+        money = input('Add the enough Money: ')
+        moneyInput += int(money)
+        if moneyInput > Coffee.Coffee.price:
+            returnMoney = moneyInput - Coffee.Coffee.price
+            print('Take your return Money:' + str(returnMoney) + 'Rs')
+    # makeCoffee()
 
-#
-# def makeCoffee():  # mack a coffee method
-if typeName == "Cappuccino":
-    ingredient=1
-    c1 = Coffee.Cappuccino(typeName, country, milk, coffeePowder, milk)
-elif typeName == "Black coffee":
-    c1 = Coffee.BlackCoffee(typeName, country, coffeePowder)
-elif typeName == "Milk Coffee":
-    c1 = Coffee.MilkCoffee(typeName, country, milk, milk)
-elif typeName == "Ice Coffee":
-    c1 = Coffee.IceCoffee(typeName, country, milk, ice, milk, sugur)
+    #
+    # def makeCoffee():  # mack a coffee method
+    if typeName == "Cappuccino":
+        ingredient = 1
+        c1 = Coffee.Cappuccino(typeName, country, milk, coffeePowder, milk)
+        c1.completeMadeTheCoffee()
+    elif typeName == "Black coffee":
+        c1 = Coffee.BlackCoffee(typeName, country, coffeePowder)
+        c1.completeMadeTheCoffee()
+    elif typeName == "Milk Coffee":
+        c1 = Coffee.MilkCoffee(typeName, country, milk, milk)
+        c1.completeMadeTheCoffee()
+    elif typeName == "Ice Coffee":
+        c1 = Coffee.IceCoffee(typeName, country, milk, ice, milk, sugur)
+        c1.completeMadeTheCoffee()
 
-print("Take your coffee from the dispathch")
-print("Coffee type : " + c1.coffeType)
-print("Country     : "+c1.country)
+    print("Take your coffee from the dispathch")
+    print("Coffee type : " + c1.coffeType)
+    print("Country     : " + c1.country)
 
-print("|---------------------------------------------------------------------|")
-print("|Available ingrediens---------->>>                                    |")
-print("|    - 1.coffee -" + str(Coffee.Coffee.coffePowder) +"                    2.Milk -"+ str(Coffee.Coffee.milk) +"                     |")
-print("|    - 3.ice    -" + str(Coffee.Coffee.ice)+"                    4.water-"+ str(Coffee.Coffee.water) +"                     |")
-print("|    - 3.sugur  -" + str(Coffee.Coffee.suger) +"                                                   |")
-print("|---------------------------------------------------------------------|")
+    print("|---------------------------------------------------------------------|")
+    print("|Available ingrediens---------->>>                                    |")
+    print("|    - 1.coffee -" + str(Coffee.Coffee.coffePowder) + "                    2.Milk -" + str(
+        Coffee.Coffee.milk) + "                     |")
+    print("|    - 3.ice    -" + str(Coffee.Coffee.ice) + "                    4.water-" + str(
+        Coffee.Coffee.water) + "                     |")
+    print("|    - 3.sugur  -" + str(Coffee.Coffee.suger) + "                                                   |")
+    print("|---------------------------------------------------------------------|")
+    moneyInput = 0
+    print("Do you want to Shopping more?")
+    print("1. yes          2. No")
+    comp = int(input('Input your openion: '))
+    if comp == 2:
+        completeBusiness = True
+print("Thank you come again!!!!!!")
